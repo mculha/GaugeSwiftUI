739923064e6e462833709e7629cc34946b78884f
mculha <melih.culha.09@gmail.com>
2023-02-14 05:57:57 +0300

    Initial Commit

---
+++ GaugeSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GaugeSwiftUI
+//
+//  Created by Melih Çulha on 14.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GaugeSwiftUI/GaugeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeSwiftUIApp.swift
+//  GaugeSwiftUI
+//
+//  Created by Melih Çulha on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct GaugeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
